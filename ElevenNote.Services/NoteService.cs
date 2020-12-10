e0a4d6860e1f72fb0dc41fbecc24a78c5c4cb23f
sreskala <sam.sagas@hotmail.com>
2020-12-10 11:20:54 -0500

    Added Create methods

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,64 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        //private field
+        private readonly Guid _userId;
+
+        //CONSTRUCTOR
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //=======CREATE NOTE=======//
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using(var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        //=====GET ALL NOTES======//
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using(var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Notes
+                    .Where(i => i.OwnerId == _userId)
+                    .Select(
+                        n =>
+                        new NoteListItem
+                        {
+                            NoteId = n.NoteId,
+                            Title = n.Title,
+                            CreatedUtc = n.CreatedUtc
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+
+    }
+}
---
+++ ElevenNoteWebMVC/Controllers/NoteController.cs
@@ -0,0 +1,63 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNoteWebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+            return View(model);
+        }
+
+        //GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Note/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            NoteService service = CreateNoteService();
+
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Note could not be created.");
+
+            return View(model);
+        }
+
+
+
+        //=========HELPER METHODS==============//
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
+    }
+}
